e8c627b59f7e8da34629261ed80b8ed7f1abbac3
Mher Hovhannisyan <mh3r99@gmail.com>
2023-01-20 21:26:54 +0400

    form submit without useRef, only FormEvent

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { ReactComponent as SearchIcon } from "assets/icon-search.svg";
 import { Button } from "components/Button";
 import styles from "./Search.module.scss";
@@ -8,17 +8,20 @@ interface SearchProps {
   onSubmit: (text: string) => void;
 }
 
+type FormFields = {
+  username: HTMLInputElement;
+};
+
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-  const searchRef = useRef<HTMLInputElement | null>(null);
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement & FormFields>
+  ) => {
     event.preventDefault();
-
-    const text = searchRef.current?.value || "";
+    const text = event.currentTarget.username.value;
 
     if (text) {
       onSubmit(text);
-      if (searchRef.current) searchRef.current.value = "";
+      event.currentTarget.reset();
     }
   };
 
@@ -29,7 +32,6 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
           <SearchIcon />
         </label>
         <input
-          ref={searchRef}
           id="search"
           type="text"
           name="username"
