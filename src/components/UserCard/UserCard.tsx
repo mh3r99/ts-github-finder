49093c476eb079d9343f2d4ea2a6454f9384c824
Mher Hovhannisyan <mh3r99@gmail.com>
2023-01-20 23:13:19 +0400

    user info

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,6 @@
+import { UserInfo } from "components/UserInfo";
 import { UserStat } from "components/UserStat";
+import { UserTitle } from "components/UserTitle";
 import { LocalGithubUser } from "types";
 import styles from "./UserCard.module.scss";
 
@@ -6,10 +8,21 @@ interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img src={props.avatar} alt={props.name} className={styles.avatar} />
+    <UserTitle created={props.created} login={props.login} name={props.name} />
+    <p className={`${styles.bio}${props.bio ? "" : ` ${styles.empty}`}`}>
+      {props.bio || "This profile has no bio"}
+    </p>
     <UserStat
       followers={props.followers}
       following={props.following}
       repos={props.repos}
+    />
+    <UserInfo
+      blog={props.blog}
+      company={props.company}
+      location={props.location}
+      twitter={props.twitter}
     />
   </div>
 );
